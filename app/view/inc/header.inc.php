b6ad29da09718d9773739f5289ae23009d78a165
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-03 21:47:45 +0100

    feat(header): Add html header to layout

---
+++ app/view/inc/header.inc.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="top-nav">
+        <ul class="navlinks">
+            <li><a href="#"></a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#" class="button">Se connecter</a></li>
+        </ul>
+    </nav>
+    <nav class="main-nav">
+        <input type="checkbox" id="check" />
+        <label for="check" class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <a href="index.php" class="logo">andraWare</a>
+        <ul class="navlinks">
+            <li><a href="#"></a></li>
+            <li><a href="#">Proposer Voyage</a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </nav>
+</header>
