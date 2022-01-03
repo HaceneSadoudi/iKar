68570e949c77ccc3fb0f2c8fb90704a33a35f80b
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-03 22:05:07 +0100

    feat: Make search form appear in home page

--- app/view/accueilSuccess.php
+++ app/view/accueilSuccess.php
@@ -1,2 +1,25 @@
-C'est l'action par défaut !
-<a href=monApplication.php?action=logout>Deconnectez vous !</a>
+<div class="container">
+    <div class="row ">
+        <div class="col-8 offset-2">
+            <div id="rechercheVoyage">
+                <form id="rechercheVoyageForm" action="" method="GET">
+                    <input hidden value="rechercheVoyage" name="action">
+                    <div class="input-group input">
+                        <input list="depart" class="form-control autocomplete" name="depart" type="text">
+                        <span data-placeholder="Départ"></span>
+                    </div>
+                    <div class="input-group input">
+                        <input list="arrivee" class="form-control autocomplete" name="arrivee" type="text">
+                        <span class="" data-placeholder="Arrivée"></span>
+                    </div>
+                    <div class="input-group button">
+                        <input type="submit" value="Recherche" class="btn">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8 offset-2" id="searchResult"></div>
+    </div>
+</div>
