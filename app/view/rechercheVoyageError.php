57e9e015a010c9e3eccffebdadf5f4a91ab21e62
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-07 23:13:03 +0100

    feat: Add an alert box for search view

--- app/view/rechercheVoyageError.php
+++ app/view/rechercheVoyageError.php
@@ -1,1 +1,5 @@
-<div id="rv-error"><div>
+<div id="rv-error">
+    <div id="bandeau" style="display : none" class="no-padding m-0 alert alert-info alert-dismissible fade show">
+        <strong>Désolé, pas de voyage qui correspond à votre recherche</strong>
+    </div>
+</div>
---
+++ app/view/rechercheVoyageSuccess.php
@@ -0,0 +1,61 @@
+
+                <div  id="bandeau" style="display : none" class="no-padding m-0 alert alert-info alert-dismissible fade show">
+                    <strong>Recherche Terminée</strong>
+                </div>
+                <ul class="search-list">
+                    <?php foreach ($context->voyages as $row) { ?>
+                        <li class="search-card">
+                            <div class="top">
+                                <div>
+                                    <div class="timeline">
+
+                                        <div class="box">
+
+                                            <div class="container d-flex">
+                                                 
+                                                <div class="lines">
+                                                    <div class="top">
+                                                        <span>Depart</span>
+                                                        <div class="dot"></div>  
+                                                        <span>15:30</span> 
+                                                    </div>                                                                        
+                                                    <div class="line"></div>
+                                                    <div class="bottom">
+                                                        <span>Depart</span>
+                                                        <div class="dot"></div>  
+                                                        <span>11:00</span> 
+                                                    </div>
+                                                </div>
+                                                
+                                        
+
+
+
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                    <h3 class="tarif">20$</h3>
+
+                                </div>
+                                <div class="bottom">
+
+                                    <div class="thumbnail">
+                                        <span class="name">John</span>
+                                    </div>
+                                    <p class="nbPlace">3 place reste</p>
+
+                                </div>
+                        </li>
+
+                    <?php } ?>
+                </ul>
+
+            
+      
+
+
+
+      
