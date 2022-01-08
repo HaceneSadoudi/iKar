6f9e28b78694720ad8e3d2fd304880e4b8c4a98b
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-08 20:00:32 +0100

    style(alert-box): add padding & margin to alert box

--- app/view/rechercheVoyageError.php
+++ app/view/rechercheVoyageError.php
@@ -1,5 +1,5 @@
 <div id="rv-error">
-    <div id="bandeau" style="display : none" class="no-padding m-0 alert alert-info alert-dismissible fade show">
+    <div id="bandeau" style="display : none" class="alert alert-info alert-dismissible fade show">
         <strong>Désolé, pas de voyage qui correspond à votre recherche</strong>
     </div>
 </div>
