d143794f6922b35157fe0012a4a84544afbdadd4
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-10 23:24:48 +0100

    feat(connexion): Add connexion action in main controller

--- app/controller/mainController.php
+++ app/controller/mainController.php
@@ -14,10 +14,34 @@ class mainController {
 	public static function rechercheVoyage($request, $context) {
 		$trajet = trajetTable::getTrajet($request['depart'], $request['arrivee']);
 		if ($trajet == NULL) return context::ERROR;
-		$context->voyages = voyageTable::getVoyagesByTrajet($trajet);		
+		$context->voyages = voyageTable::getVoyagesByTrajet($trajet);  
 		if ($context->voyages == false) {
 			return context::ERROR;
 		}
+		return context::SUCCESS;
+	}
+
+	public static function connexion($request, $context) {
+
+		if (isset($request['pseudo']) && isset($request['pass'])) {
+			$pseudo = $request['pseudo'];
+			$pass = $request['pass'];
+
+			if (!empty($pseudo) && !empty($pass)) {
+				$exist = utilisateurTable::getUserByLoginAndPass($pseudo, $pass);
+				echo json_encode($exist);
+				if ($exist) {
+					$context->setSessionAttribute('is_logged', 'true');
+					$context->setSessionAttribute('id', $exist->id);
+					$context->setSessionAttribute('identifiant', $exist->identifiant);
+					$context->setSessionAttribute('nom', $exist->nom);
+					$context->setSessionAttribute('prenom', $exist->prenom);
+					$context->setSessionAttribute('image', $exist->avatar);
+				}
+				return context::NONE;
+			}
+		}
+
 		return context::SUCCESS;
 	}
 }
