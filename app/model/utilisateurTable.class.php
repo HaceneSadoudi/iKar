ae0d9659724c8184348d6f2f509df89f5860ce57
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-17 23:40:45 +0100

    feat: Add new user to database

--- app/model/utilisateurTable.class.php
+++ app/model/utilisateurTable.class.php
@@ -5,9 +5,7 @@ require_once "utilisateur.class.php";
 class utilisateurTable {
 
     public static function getUserByLoginAndPass($login, $pass) {
-        
         $em = dbconnection::getInstance()->getEntityManager();
-        
         $userRepository = $em->getRepository('utilisateur');
         $user = $userRepository->findOneBy(array(
             'identifiant' => $login,
@@ -24,5 +22,16 @@ class utilisateurTable {
         $user = $userRepository->findOneBy(array('id' => $id));
         if ($user == false) echo "<br>Erreur SQL<br>";
         return $user;
+    }
+
+    public static function setUser($identifiant, $pass, $nom, $prenom) {
+        $em = dbconnection::getInstance()->getEntityManager();
+        $ut = new utilisateur();
+        $ut->identifiant = $identifiant;
+        $ut->pass = $pass;
+        $ut->nom  = $nom;
+        $ut->prenom = $prenom;
+        $em->persist($ut);
+        $em->flush();
     }
 }
