aa723f92e8fc31841b2e755d415a632b86b3df35
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-18 03:15:57 +0100

    fix: Change the icon when showing/hiding password

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ $(document).ready(function () {
   // PASSWORD HIDE/SHOW TOGGLE
   $(document).on("click", ".pwdToggle", (e) => {
     var pwdInput = $(e.currentTarget).parent().find("input");
+    var icon = e.target == e.currentTarget 
+        ? $(e.target).children().first() // e.target == e.currentTarget == <button>
+        : $(e.target); // e.target == <i> AND e.currentTarget == <button>
     if (pwdInput.attr("type") == "password") {
       pwdInput.prop("type", "text");
+      icon.addClass("fa-eye-slash");      
     } else {
       pwdInput.prop("type", "password");
+      icon.removeClass("fa-eye-slash");
     }
   });
 });
