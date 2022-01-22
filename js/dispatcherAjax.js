b026a026a84cbe272ef608d32f8030fee8cdad15
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-22 18:40:38 +0100

    feat: Prevent ajax call when input values are not valid

--- js/dispatcherAjax.js
+++ js/dispatcherAjax.js
@@ -1,19 +1,16 @@
 $(document).ready(function () {
-
   /* ############################################################# */
   /* #######################  HANDLE NOTIF  ###################### */
   /* ############################################################# */
   var notif = (type, msg, duration = 4000) => {
-    var types = ['error', 'success', 'warning', 'info'];
-    for(const e of types) {
-      $("#bandeau").removeClass(e);console.log(e);
+    var types = ["error", "success", "warning", "info"];
+    for (const e of types) {
+      $("#bandeau").removeClass(e);
     }
     $("#bandeau").addClass(type);
     $("#bandeau .content").text(msg);
     $("#bandeau").show().delay(duration).fadeOut();
-  }
-
-
+  };
 
   /* ############################################################# */
   /* ####################  RECHERCHE VOYAGE  ##################### */
@@ -41,14 +38,20 @@ $(document).ready(function () {
       },
       error: function (jqXhr, textStatus, errorThrown) {
         console.log(errorThrown);
-        notif("error", "Une erreur s'est produite. Veuillez réessayer plus tard");
-       /*  $("#bandeau .content").text("Désolé, une erreur s'est produite");
+        notif(
+          "error",
+          "Une erreur s'est produite. Veuillez réessayer plus tard"
+        );
+        /*  $("#bandeau .content").text("Désolé, une erreur s'est produite");
         $("#bandeau").show().delay(4000).fadeOut(); */
       },
     });
   });
 
-  // NAVBAR CONNEXION
+  //
+  /* ############################################################# */
+  /* ####################  NAVBAR CONNEXION  ##################### */
+  /* ############################################################# */
   $(document).on("click", "#connexion-btn", () => {
     $.ajax({
       // url : 'monApplicationAjax.php?action=testVoyage&depart='+formData['depart']+'&arrivee='+formData['arrivee'],
@@ -88,7 +91,10 @@ $(document).ready(function () {
       },
       error: function (jqXhr, textStatus, errorThrown) {
         console.log(errorThrown);
-        notif("error", "Une erreur s'est produite. Veuillez réessayer plus tard");
+        notif(
+          "error",
+          "Une erreur s'est produite. Veuillez réessayer plus tard"
+        );
       },
     });
   });
@@ -100,43 +106,49 @@ $(document).ready(function () {
     e.preventDefault();
 
     var formData = $("#connexionForm").serialize();
-
-    $.ajax({
-      // url : 'monApplicationAjax.php?action=testVoyage&depart='+formData['depart']+'&arrivee='+formData['arrivee'],
-      url: "dispatcherAjax.php?action=connexion",
-      type: "POST",
-      data: formData,
-      dataType: "text",
-      success: function (response, statut) {
-        if (response == 0) {
-         /*  $("#bandeau").removeClass("success").addClass("error");
+    if (loginValidation())
+      $.ajax({
+        // url : 'monApplicationAjax.php?action=testVoyage&depart='+formData['depart']+'&arrivee='+formData['arrivee'],
+        url: "dispatcherAjax.php?action=connexion",
+        type: "POST",
+        data: formData,
+        dataType: "text",
+        success: function (response, statut) {
+          if (response == 0) {
+            /*  $("#bandeau").removeClass("success").addClass("error");
           $("#bandeau .content").text(
             "L'identifiant ou le mot de passe est invalide"
           );
           $("#bandeau").show().delay(4000).fadeOut(); */
-          notif("error", "L'identifiant ou le mot de passe est invalide");
-        } else if (response == 1) {
-          /* $("#bandeau").removeClass("error").addClass("success");
+            notif("error", "L'identifiant ou le mot de passe est invalide");
+          } else if (response == 1) {
+            /* $("#bandeau").removeClass("error").addClass("success");
           $("#bandeau .content").text("Vous êtes connecté");
           $("#bandeau").show().delay(4000).fadeOut(); */
-          notif("success", "Vous êtes connecté");
-          setTimeout(function () {
-            window.location.href = "index.php";
-          }, 2500);
-        } else {
-          /* $("#bandeau").removeClass("success").addClass("error");
+            notif("success", "Vous êtes connecté");
+            setTimeout(function () {
+              window.location.href = "index.php";
+            }, 2500);
+          } else {
+            /* $("#bandeau").removeClass("success").addClass("error");
           $("#bandeau .content").text(
             "Une erreur s'est produite. Veuillez réessayer plus tard"
           );
           $("#bandeau").show().delay(4000).fadeOut(); */
-          notif("error", "Une erreur s'est produite. Veuillez réessayer plus tard");
-        }
-      },
-      error: function (jqXhr, textStatus, errorThrown) {
-        console.log(errorThrown);
-        notif("error", "Une erreur s'est produite. Veuillez réessayer plus tard");
-      },
-    });
+            notif(
+              "error",
+              "Une erreur s'est produite. Veuillez réessayer plus tard"
+            );
+          }
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+          console.log(errorThrown);
+          notif(
+            "error",
+            "Une erreur s'est produite. Veuillez réessayer plus tard"
+          );
+        },
+      });
   });
 
   /* ############################################################# */
@@ -152,42 +164,48 @@ $(document).ready(function () {
   $("#page_maincontent").on("submit", "#inscriptionForm", (e) => {
     e.preventDefault();
     var fdata = $("#inscriptionForm").serialize();
-
-    $.ajax({
-      // url : 'monApplicationAjax.php?action=testVoyage&depart='+formData['depart']+'&arrivee='+formData['arrivee'],
-      url: "dispatcherAjax.php?action=inscription",
-      type: "POST",
-      data: fdata,
-      dataType: "text",
-      success: function (response, statut) {
-        if (response == 0) {
-          /* $("#bandeau").removeClass("success").addClass("error");
+    if (signUpValidation())
+      $.ajax({
+        // url : 'monApplicationAjax.php?action=testVoyage&depart='+formData['depart']+'&arrivee='+formData['arrivee'],
+        url: "dispatcherAjax.php?action=inscription",
+        type: "POST",
+        data: fdata,
+        dataType: "text",
+        success: function (response, statut) {
+          if (response == 0) {
+            /* $("#bandeau").removeClass("success").addClass("error");
           $("#bandeau .content").text(
             "L'identifiant de l'utilisateur existe déjà"
           );
           $("#bandeau").show().delay(4000).fadeOut(); */
-          notif("error", "L'identifiant de l'utilisateur existe déjà");
-        } else if (response == 11 || response == 10) {
-          /* $("#bandeau").removeClass("error").addClass("success");
+            notif("error", "L'identifiant de l'utilisateur existe déjà");
+          } else if (response == 11 || response == 10) {
+            /* $("#bandeau").removeClass("error").addClass("success");
           $("#bandeau .content").text("Votre compte a été créé avec succès");
           $("#bandeau").show().delay(4000).fadeOut(); */
-          notif("success", "Votre compte a été créé avec succès");
-          setTimeout(function () {
-            window.location.href = "index.php";
-          }, 2500);
-        } else {
-          /* $("#bandeau").removeClass("success").addClass("error");
+            notif("success", "Votre compte a été créé avec succès");
+            setTimeout(function () {
+              window.location.href = "index.php";
+            }, 2500);
+          } else {
+            /* $("#bandeau").removeClass("success").addClass("error");
           $("#bandeau .content").text(
             "Une erreur s'est produite. Veuillez réessayer plus tard"
           );
           $("#bandeau").show().delay(4000).fadeOut(); */
-          notif("error", "Une erreur s'est produite. Veuillez réessayer plus tard");
-        }
-      },
-      error: function (jqXhr, textStatus, errorThrown) {
-        console.log(errorThrown);
-        notif("error", "Une erreur s'est produite. Veuillez réessayer plus tard");
-      },
-    });
+            notif(
+              "error",
+              "Une erreur s'est produite. Veuillez réessayer plus tard"
+            );
+          }
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+          console.log(errorThrown);
+          notif(
+            "error",
+            "Une erreur s'est produite. Veuillez réessayer plus tard"
+          );
+        },
+      });
   });
 });
