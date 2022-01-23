592fcad47475fd57620fa9bee826db0f5764d223
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-23 23:18:07 +0100

    feat(voyage): Add voyage to database

--- app/model/voyageTable.class.php
+++ app/model/voyageTable.class.php
@@ -12,4 +12,18 @@ class voyageTable {
         return $voyages;
     }
 
+    public static function setVoyage($conducteur, $trajet, $prix, $nbPlace, $heureDepart, $contraintes) {
+        $em = dbconnection::getInstance()->getEntityManager();
+        $v = new voyage();
+        $v->conducteur = $conducteur;
+        $v->trajet = $trajet;
+        $v->tarif = $prix;
+        $v->nbPlace = $nbPlace;
+        $v->heureDepart = $heureDepart;
+        $v->contraintes = $contraintes;
+
+        $em->persist($v);
+        $em->flush();
+    }
+
 }
