21e2011fca1a98f3e967907b6342464f437f0a14
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-25 22:58:50 +0100

    feat: Store a user reservation to database

--- app/model/reservationTable.class.php
+++ app/model/reservationTable.class.php
@@ -11,4 +11,13 @@ class reservationTable {
         $reservations = $reservationRepo->findBy(array('voyage' => $voyage));
         return $reservations;
     }
+
+    public static function setReservation($voyage, $voyageur) {
+        $em = dbconnection::getInstance()->getEntityManager();
+        $reservation = new reservation();
+        $reservation->voyage = voyageTable::getVoaygeById($voyage);;
+        $reservation->voyageur = voyageTable::getVoaygeByConducteur($voyageur);
+        $em->persist($reservation);
+        $em->flush();
+    }
 }
