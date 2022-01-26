0641e1b1ed8ffc32ef740b50c28b79d9aaf8f089
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-26 22:57:04 +0100

    feat(module): Add length validation check to the validation module

--- js/validation.js
+++ js/validation.js
@@ -7,3 +7,11 @@ export function validateEmail(data) {
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return regex.test(data.trim());
 }
+
+export function validateLength(data, min=1, max=Number.MAX_SAFE_INTEGER) {
+    data = data.trim();
+    if(data.length < min) return false;
+    if(data.length > max) return false;
+    return true;
+}
+
