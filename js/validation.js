c87195b62dab1decddb627168722009b08d2d203
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-26 22:56:16 +0100

    feat(module): Add email validation check to the validation module

--- js/validation.js
+++ js/validation.js
@@ -2,3 +2,8 @@ export function validatePassword(data) {
     let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     return regex.test(data.trim());
 };
+
+export function validateEmail(data) {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(data.trim());
+}
