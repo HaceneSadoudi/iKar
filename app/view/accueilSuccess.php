d1b1f2ea9b80b9385f799591069e00ec67afdf18
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-28 22:49:54 +0100

    style: Stop showing search result in index view
    The index search form will call another action to show the result (Look at Js script)

--- app/view/accueilSuccess.php
+++ app/view/accueilSuccess.php
@@ -1,42 +1,24 @@
-<div class="">
+<div id="hero_section">
     <div class="container">
         <div class="row justify-content-end">
             <div class="col-4">
-                <form action="" method="POST" id="rechercheVoyageForm">
-                    <input hidden value="rechercheVoyage" name="action">
-                    <div class="step step-one active">
-                        <div class="input-group">
-                            <input type="text" class="form-control" name="depart" id="depart" placeholder="Ville de départ">
-                        </div>
-                        <div class="input-group">
-                            <input type="text" class="form-control" name="arrivee" id="arrivee" placeholder="Ville d'arrivée">
-                        </div>
-                        <div class="input-group">
-                            <input type="text" class="form-control" name="nbplaces" id="nbplaces" placeholder="Nombre de places">
-                        </div>
-                        <div class="input-group">
-                            <button type="submit" class="next-btn search-btn">Rechercher</button>
-                        </div>                        
+                <form action="" method="POST" id="hero_search_form">
+                    <input hidden value="searchVoyage" name="action">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="depart" id="depart" placeholder="Ville de départ">
                     </div>
-                    <div class="step step-four">
-                        <a href="javascrip:void(0)" class="next-btn">
-                            <i class="fa fa-search" aria-hidden="true"></i>
-                            <span class="depart"></span>
-                            <i class="fa fa-caret-right" aria-hidden="true"></i>
-                            <span class="arrivee"></span>
-                        </a>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="arrivee" id="arrivee" placeholder="Ville d'arrivée">
+                    </div>
+                    <div class="input-group">
+                        <input type="number" class="form-control" name="nbplaces" id="nbplaces" placeholder="Nombre de places">
+                    </div>
+                    <div class="input-group">
+                        <button type="submit" class="next-btn search-btn">Rechercher</button>
                     </div>
                 </form>
-
             </div>
             <div class="col-8 bg-img"></div>
         </div>
     </div>
 </div>
-<section class="main-section">
-    <div class="container ">
-        <div class="row">
-            <div class="col-10 offset-1" id="searchResult"></div>
-        </div>
-    </div>
-</section>
