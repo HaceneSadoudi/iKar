39aba0b9bcce3d9bdf99b9fb1e28a8c6641e0874
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-28 23:02:53 +0100

    chore: update scripts type and add animate css cdn

--- app/layout/layout.php
+++ app/layout/layout.php
@@ -9,6 +9,7 @@
   </title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   <link rel="stylesheet" href="css/mainStyle.css">
   <link rel="stylesheet" href="css/style.css">
 </head>
@@ -38,11 +39,12 @@
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="js/lib.js" type="text/javascript"></script>
+  <script src="validation.js"></script>
+  <script src="js/lib.js"></script>
   <script src="https://apis.google.com/js/platform.js" async defer></script>
-  <script src="js/main.js" type="text/javascript"></script>
-  <script src="js/app.js" type="text/javascript"></script>
-  <script src="js/dispatcherAjax.js" type="text/javascript"></script>
+  <script src="js/main.js"></script>
+  <script src="js/app.js" type="module"></script>
+  <script src="js/dispatcherAjax.js" type="module"></script>
 </body>
 
 </html>
