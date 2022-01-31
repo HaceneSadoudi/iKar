efea0333aa0223990c3c372d7fcb5164e933e635
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-31 23:59:55 +0100

    fix: fix the regex for username

--- js/validation.js
+++ js/validation.js
@@ -30,7 +30,7 @@ export function validateName(data) {
 }
 
 export function validateUsername(data) {
-    const regex = /^(?:[A-Za-z][A-Za-z0-9]*){4,}$/;
+    const regex = /^(?:[A-Za-z])[A-Za-z0-9]{3,}$/;
     return regex.test(data);
 }
 
