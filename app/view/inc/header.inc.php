0ef4418dce8d23754b901526dadb8692515a1607
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-03 23:27:42 +0100

    style: Add top navbar for personal informations

--- app/view/inc/header.inc.php
+++ app/view/inc/header.inc.php
@@ -1,6 +1,18 @@
 <header>
-    <nav class="top-nav">
-        <ul class="navlinks">
+    <nav class="navbar p-0">
+        <div class="navbar-inner w-100">
+            <div class="d-flex justify-content-between align-items-center top-nav">
+                <ul class="personal-info d-flex">
+                    <li class="personal-info-item"><i class="fa fa-map-marker" aria-hidden="true"></i> 3 rue galante, Avignon</li>
+                    <li class="personal-info-item"><i class="fa fa-phone" aria-hidden="true"></i> (+33) 758 765 104</li>
+                    <li class="personal-info-item"><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:sadoudi2019@gmail.com">sadoudi2019@gmail.com</a></li>
+                </ul>
+                <ul class="social-media d-flex">
+                    <li class="social-media-item"><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                    <li class="social-media-item"><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li class="social-media-item"><a href=""><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
             <li><a href="#"></a></li>
             <li><a href="#">Projects</a></li>
             <li><a href="#" class="button">Se connecter</a></li>
@@ -17,5 +29,8 @@
             <li><a href="#">Proposer Voyage</a></li>
             <li><a href="#"></a></li>
         </ul>
+        </div>
+
+
     </nav>
 </header>
