ba99e1116c479c19a7122cd8731b5c21708d4728
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-03 23:21:00 +0100

    style: Change the login view style

--- app/view/connexionSuccess.php
+++ app/view/connexionSuccess.php
@@ -1,65 +1,36 @@
-<div class="sign-container" id="sign-container">
-    <div class="form-container sign-up-container">
-        <form action="#" id="inscriptionForm">
-            <h1>Inscription</h1>
-            <div class="social-container">
-                <div id="g-sign-up" class="g-signin2" data-onsuccess="onSignIn"></div>
+<div id="login-container">
+    <div class="row m-0">
+        <div class="col-lg-7 login-form-container">
+            <div class="login-form-wrapper">
+                <h1 class="title">S'identifier</h1>
+                <form id="login-form" class="">
+                    <div class="input-group">
+                        <div class="input-group-row">
+                            <input name="identifiant" class="input-lg w-100" type="text" placeholder="Identifiant" />
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group-row">
+                            <input name="pass" class="input-lg w-100" type="password" placeholder="Mot de passe" />
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group">
+                        <input class="button button-lg" type="submit" value="Se connecter">
+                    </div>
+                    <div class="input-group mt-4">
+                        <p>Pas encore membre? <a href="" class="inscription-btn" >S'inscrire</a></p>
+                    </div>
+                </form>
             </div>
-            <span>Ou utilisez votre email pour l'inscription</span>
-            <div class="input-group">
-                <input name="rnom" type="text" placeholder="Nom" />
-                <span class="alert-message">Error</span>
-            </div>
-            <div class="input-group">
-                <input name="rprenom" type="text" placeholder="Prénom" />
-                <span class="alert-message">Error</span>
-            </div>
-            <div class="input-group">
-                <input name="ridentifiant" type="text" placeholder="Pseudo" />
-                <span class="alert-message">Error</span>
-            </div>
-            <div class="input-group">
-                <input type="password" name="rpass" class="password" placeholder="Mot de passe" />
-                <span class="alert-message">Error</span>
-                <button type="button" class="pwdToggle"><i class="fa fa-eye" aria-hidden="true"></i></button>
-            </div>
-
-            <button class="mt-2">Inscription</button>
-        </form>
-    </div>
-    <div class="form-container sign-in-container">
-        <form action="" id="connexionForm" method="POST">
-            <h1>Connexion</h1>
-            <div class="social-container">
-                <div id="g-sign-in" class="g-signin2" data-onsuccess="onSignIn"></div>
-            </div>
-            <span>Ou utilisez votre email</span>
-            <!-- Identifiant -->
-            <div class="input-group">
-                <input type="text" placeholder="Identifiant" name="lidentifiant" />
-                <span class="alert-message">Error</span>
-            </div>
-            <!-- Password -->
-            <div class="input-group">
-                <input type="password" class="password" placeholder="Mot de passe" name="lpass" />
-                <span class="alert-message">Error</span>
-                <button type="button" class="pwdToggle"><i class="fa fa-eye" aria-hidden="true"></i></button>
-            </div>
-            <a href="#">Mot de pass oublier?</a>
-            <button type="submit" id="seConnecter">Se connecter</button>
-        </form>
-    </div>
-    <div class="overlay-container">
-        <div class="overlay">
-            <div class="overlay-panel overlay-left">
-                <h1>Bienvenu!</h1>
-                <p>Pour restez connecter avec nous, connectez-vous avec vous informations personnelles</p>
-                <button class="ghost" id="signInButton">Se connecter</button>
-            </div>
-            <div class="overlay-panel overlay-right">
-                <h1>Salut!</h1>
-                <p>Entrez vous données personnelles et commencez votre voyage avec nous.</p>
-                <button class="ghost" id="signUpButton">Inscription</button>
+        </div>
+        <div class="col-lg-5 login-info-container">
+            <div class="login-info-wrapper">
             </div>
         </div>
     </div>
