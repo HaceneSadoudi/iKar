3ebdb004a0710127a29384efc737954cbb8710c8
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-06 17:45:32 +0100

    fix: Limite the name to 20 characters with regex

--- js/validation.js
+++ js/validation.js
@@ -1,60 +1,60 @@
 export function validatePassword(data) {
-    let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-    return regex.test(data.trim());
-};
-
-export function validateEmail(data) {
-    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return regex.test(data.trim());
+  let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+  return regex.test(data.trim());
 }
 
-export function validateLength(data, min=1, max=Number.MAX_SAFE_INTEGER) {
-    data = data.trim();
-    if(data.length < min) return false;
-    if(data.length > max) return false;
-    return true;
+export function validateEmail(data) {
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(data.trim());
+}
+
+export function validateLength(data, min = 1, max = Number.MAX_SAFE_INTEGER) {
+  data = data.trim();
+  if (data.length < min) return false;
+  if (data.length > max) return false;
+  return true;
 }
 
 export function validatePhone(data) {
-    let regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    return regex.test(data.trime());
+  let regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return regex.test(data.trime());
 }
 
 export function validateNumber(data) {
-    return !isNaN(data.trim());
+  return !isNaN(data.trim());
 }
 
 export function validateName(data) {
-    let regex = /^[a-zA-Z]{2,}$/;
-    return regex.test(data);
+  let regex = /^[a-zA-Z]{2,20}$/;
+  return regex.test(data);
 }
 
 export function validateUsername(data) {
-    const regex = /^(?:[A-Za-z])[A-Za-z0-9]{3,20}$/;
-    return regex.test(data);
+  const regex = /^(?:[A-Za-z])[A-Za-z0-9]{3,20}$/;
+  return regex.test(data);
 }
 
 export function containsWhitespace(data) {
-    const regex = /^(?=.*\s)/;
-    return regex.test(data);
+  const regex = /^(?=.*\s)/;
+  return regex.test(data);
 }
 
 export function containsUppercase(data) {
-    const regex = /^(?=.*[A-Z])/;
-    return regex.test(data);
+  const regex = /^(?=.*[A-Z])/;
+  return regex.test(data);
 }
 
 export function containsLowercase(data) {
-    const regex = /^(?=.*[a-z])/;
-    return regex.test(data);
+  const regex = /^(?=.*[a-z])/;
+  return regex.test(data);
 }
 
 export function containsDigit(data) {
-    const regex = /^(?=.*[0-9])/;
-    return regex.test(data);
+  const regex = /^(?=.*[0-9])/;
+  return regex.test(data);
 }
 
 export function containsSymbol(data) {
-    const regex = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
-    return regex.test(data);
+  const regex = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
+  return regex.test(data);
 }
