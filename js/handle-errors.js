efad998cd4e51d8c89878dd98969de40af521edb
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-06 18:40:47 +0100

    feat: Show form input success when it's valid

--- js/handle-errors.js
+++ js/handle-errors.js
@@ -2,7 +2,7 @@
 /* ####################  SHOW INPUT ERROR  ##################### */
 /* ############################################################# */
 var showError = function (input, message = "") {
-  const input_group = input.parents('.input-group');
+  const input_group = input.parents(".input-group");
   const small = input_group.find("small");
   input_group.removeClass("success").addClass("error");
   small.text(message);
@@ -10,7 +10,16 @@ var showError = function (input, message = "") {
 /* ############################################################# */
 /* ####################  HIDE INPUT ERROR  ##################### */
 /* ############################################################# */
-var hideError = function(input) {
-  const input_group = input.parents('.input-group');
+var hideError = function (input) {
+  const input_group = input.parents(".input-group");
   input_group.removeClass("error");
-}
+};
+/* ############################################################# */
+/* ####################  SHOW INPUT SUCCESS  ################### */
+/* ############################################################# */
+var showSuccess = function (input) {
+  const input_group = input.parents(".input-group");
+  input_group.removeClass("error").addClass("success");
+};
+
+export { showError, hideError, showSuccess };
