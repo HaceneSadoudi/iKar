e12e4b4ee6f6058c2c9063f7c9650bba8a38bacb
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-14 23:01:49 +0100

    style: Add view for showing searched results

---
+++ app/view/rechercheVoyageResultSuccess.php
@@ -0,0 +1,27 @@
+<ul class="search-list">
+    <?php $count = 0;
+    foreach ($context->voyages as $row) {
+        if ($count % 2 == 0) { ?>
+            <div class="row justify-content-center">
+            <?php } ?>
+            <div class="col-6">
+                <li class="search-card">
+                    <div href="" class="reserve-btn">Resérver</div>
+                    <div class="content">
+                        <div>
+                            <p><b>Heure de départ : <?php echo $row['heureDepart'], ":00" ?></b></p>
+                            <p><b>Contraintes : <?php echo $row['contraintes'] ?></b></p>
+                            <p>Il rest que <?php echo $row['nbPlace'] ?> place(s) </p>
+                        </div>
+                        <div>
+                            <h2><?php echo $row['tarif'] ?> €</h2>
+                        </div>
+                    </div>
+                </li>
+            </div>
+            <?php if ($count % 2 != 0) { ?>
+            </div>
+        <?php }
+            $count++; ?>
+    <?php } ?>
+</ul>
