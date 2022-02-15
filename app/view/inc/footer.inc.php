4ccefdeccd94f3f2ddc1d07ea84485a5a196a2d3
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-15 23:15:24 +0100

    style: Add footer view

---
+++ app/view/inc/footer.inc.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="container py-5">
+        <div class="row py-4">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <p class="font-italic text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry.dummy text of the printing and typesetting industry</p>
+                <ul class="list-inline mt-4 social-media">
+                    <li class="social-media-item list-inline-item"><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                    <li class="social-media-item list-inline-item"><a href=""><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+                    <li class="social-media-item list-inline-item"><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-6 mb-4">
+                <h6 class="text-uppercase font-weight-bold mb-4">Entreprise</h6>
+                <ul class="mb-0">
+                    <li class="mb-2"><a href="" class="text-muted">À Propos</a></li>
+                    <li class="mb-2"><a href="" class="text-muted">Centre d'aide</a></li>
+                    <li class="mb-2"><a href="" class="text-muted">Terms et Conditions</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-6 mb-4">
+                <h6 class="text-uppercase font-weight-bold mb-4">Voyage</h6>
+                <ul class="mb-0">
+                    <li class="mb-2"><a href="" class="text-muted">Villes</a></li>
+                    <li class="mb-2"><a href="" class="text-muted">Hotels</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h6 class="text-uppercase font-weight-bold mb-4">Newsletter</h6>
+                <p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At itaque temporibus.</p>
+                <div class="p-1 rounded-group">
+                    <div class="input-group">
+                        <input class="form-control border-0 shadow-0" type="email" placeholder="Entrez votre adresse mail">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-link"><i class="fa fa-paper-plane"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-light py-4" id="copyright-wrapper">
+        <div class="container text-center">
+            <p class="mb-0 text-muted py-2">Copyright 2016-<?php echo date("Y"); ?> by iKar. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
