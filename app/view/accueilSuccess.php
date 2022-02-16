9512e5c32ad121553f9c2e4cbfd305d5631554aa
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-16 21:47:24 +0100

    style: Add search form on home page for fast search

--- app/view/accueilSuccess.php
+++ app/view/accueilSuccess.php
@@ -1,7 +1,7 @@
-<div id="hero_section">
+<div id="hero-section" class="bg-img">
     <div class="container">
         <div class="row justify-content-end">
-            <div class="col-4">
+            <!-- <div class="col-4">
                 <form action="" method="POST" id="hero_search_form">
                     <input hidden value="searchVoyage" name="action">
                     <div class="input-group">
@@ -17,8 +17,101 @@
                         <button type="submit" class="next-btn search-btn">Rechercher</button>
                     </div>
                 </form>
+            </div> -->
+            <div class="col-lg-5 hero-form-container">
+                <!--                 <div class="hero-form-wrapper">
+                    <form id="hero-form" class="">
+                        <div class="">
+                            <div class="input-group">
+                                <div class="input-group-row">
+                                    <input name="depart" type="text" placeholder="Ville de départ" autofocus />
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                                </div>
+                                <small class="alert-message">Error</small>
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-row">
+                                    <input name="arrivee" type="text" placeholder="Ville d'arrivée" autofocus />
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                                </div>
+                                <small class="alert-message">Error</small>
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-row">
+                                    <input name="nbplaces" type="text" placeholder="Nombre de places" />
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                                </div>
+                                <small class="alert-message">Error</small>
+                            </div>
+                            <div class="input-group">
+                                <input class="button" type="submit" value="Rechercher">
+                            </div>
+                        </div>
+                    </form>
+                </div> -->
             </div>
-            <div class="col-8 bg-img"></div>
+            <div class="col-7"></div>
         </div>
     </div>
 </div>
+<section id="main-section">
+    <div class="container">
+        <div class="row justify-content-end">
+            <!-- <div class="col-4">
+                <form action="" method="POST" id="hero_search_form">
+                    <input hidden value="searchVoyage" name="action">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="depart" id="depart" placeholder="Ville de départ">
+                    </div>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="arrivee" id="arrivee" placeholder="Ville d'arrivée">
+                    </div>
+                    <div class="input-group">
+                        <input type="number" class="form-control" name="nbplaces" id="nbplaces" placeholder="Nombre de places">
+                    </div>
+                    <div class="input-group">
+                        <button type="submit" class="next-btn search-btn">Rechercher</button>
+                    </div>
+                </form>
+            </div> -->
+            <div class="col-lg-12 main-form__container">
+                <div class="main-form__wrapper">
+                    <form id="main-form" class="">
+                        <div class="main-form__inner-wrapper">
+                            <div class="input-group">
+                                <small class="">Ville de départ</small>
+                                <div class="input-group-row m-0">
+                                    <input name="depart" class="input-sm rounded-0" type="text" placeholder="Ex: Paris, Avignon, Lille..." />
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <small class="">Ville d'arrivée</small>
+                                <div class="input-group-row m-0">
+                                    <input name="arrivee" class="input-sm rounded-0" type="text" placeholder="Ex: Paris, Avignon, Lille..." />
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <small class="">Nombre de places</small>
+                                <div class="input-group-row m-0">
+                                    <input name="nbplaces" class="input-sm rounded-0" type="text" placeholder="Ex: 1, 2, 3" />
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                    <i class="fa fa-check-circle" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <input class="button2 w-100 rounded-0" type="submit" value="Rechercher">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
