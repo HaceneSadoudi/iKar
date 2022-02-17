0a47e73f9c6697a09c81836f52d239a6b67c398d
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-17 23:58:07 +0100

    chore: update external js scripts in the layout

--- app/layout/layout.php
+++ app/layout/layout.php
@@ -46,7 +46,8 @@
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="validation.js"></script>
+  <script src="js/handle-errors.js"></script>
+  <script src="js/validation.js"></script>
   <script src="js/lib.js"></script>
   <script src="https://apis.google.com/js/platform.js" async defer></script>
   <script src="js/main.js"></script>
