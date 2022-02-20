ae854411452db89bc574c342a943c4e9d7bec4de
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-20 23:41:36 +0100

    feat: Handle the main slider with slider library

--- js/main.js
+++ js/main.js
@@ -56,6 +56,26 @@ jQuery(document).ready(function ($) {
         }
       }
     });
+
+  var siteSliderRange = function () {
+    $("#slider-range").slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [75, 300],
+      slide: function (event, ui) {
+        $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      },
+    });
+    $("#amount").val(
+      "$" +
+        $("#slider-range").slider("values", 0) +
+        " - $" +
+        $("#slider-range").slider("values", 1)
+    );
+  };
+  // siteSliderRange();
+
   var siteDatePicker = function () {
     if ($(".datepicker").length > 0) {
       $(".datepicker").datepicker();
