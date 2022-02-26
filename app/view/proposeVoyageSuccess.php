6730b6d00a8e44e4ae3d316e4fdd3505cb590ac0
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-02-26 23:26:24 +0100

    style: update suggest ride UI

--- app/view/proposeVoyageSuccess.php
+++ app/view/proposeVoyageSuccess.php
@@ -1,53 +1,68 @@
-<div class="propose-container" id="propose-container">
-    <div class="form-container sign-up-container">
-        <form action="#" id="proposeForm">
-            <h1>Propser Un Voyage</h1>
-            <div class="input-group">
-                <input name="depart" type="text" placeholder="Depart" value="Paris" />
-                <span class="alert-message">Error</span>
+<div id="custom-form__section">
+    <div class="row m-0">
+        <div class="col-lg-7 custom-form__container">
+            <div class="custom-form__wrapper">
+                <h1 class="title">Proposer un voyage</h1>
+                <form id="js-propose-form" class="custom-form" autocomplete="off">
+                    <div class="input-group">
+                        <div class="input-group-row form-group">
+                            <select class="" id="" required>
+                                <option value="" hidden>Ville de départ</option>
+                                <?php foreach ($context->allCities as $row) { ?>
+                                    <option value="<?php echo $row['depart'] ?>"><?php echo $row['depart'] ?></option>
+                                <?php } ?>
+                            </select>
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group-row">
+                            <select class="input-lg w-100" name="depart" required>
+                                <option value="" disabled selected hidden>Ville d'arrivée </option>
+                                <?php foreach ($context->allCities as $row) { ?>
+                                    <option value="<?php echo $row['depart'] ?>"><?php echo $row['depart'] ?></option>
+                                <?php } ?>
+                            </select>
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group-row">
+                            <input name="nbplaces" class="input-lg w-100" type="number" max="15" placeholder="Nombre de places" required />
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group-row">
+                            <input name="nbplaces" class="input-lg w-100" type="number" placeholder="Prix par place" required />
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group-row">
+                            <input name="depart" class="input-lg w-100" type="text" placeholder="Contraintes" />
+                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true"></i>
+                        </div>
+                        <small class="alert-message">Error</small>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input class="button button-lg" id="submit" type="button" value="Valider">
+                    </div>
+                </form>
             </div>
-            <div class="input-group">
-                <input name="arrivee" type="text" placeholder="Arrivée" value="Nice" />
-                <span class="alert-message">Error</span>
+        </div>
+        <div class="col-lg-5 custom-info__container">
+            <div class="custom-info__wrapper">
             </div>
-            <div class="input-group">
-                <input name="heureDepart" type="number" placeholder="Heure De Départ" value="16" />
-                <span class="alert-message">Error</span>
-            </div>
-            <div class="input-group">
-                <input name="prix" type="number" placeholder="Prix" value="30" />
-                <span class="alert-message">Error</span>
-            </div>
-            <div class="input-group">
-                <input name="nbPlace" type="number" placeholder="Nombre De Places" value="3" />
-                <span class="alert-message">Error</span>
-            </div>
-            <div class="input-group">
-                <div class="checkbox">
-                    <label>
-                        <input name="contraintes" type="checkbox" value="PHP">
-                        Animaux non acceptés
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input name="contraintes" type="checkbox" value="Java">
-                        Boissons non acceptées
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input name="contraintes" type="checkbox" value="Java">
-                        Non Fumeur
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input name="contraintes" checked type="hidden" value="Aucune contraites">
-                    </label>
-                </div>
-            </div>
-            <button type="submit" class="mt-2">Valider</button>
-        </form>
+        </div>
     </div>
 </div>
